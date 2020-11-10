fef59ee50b8238be5342a57225416f4397a6c6e1
Irshak10 <74018714+Irshak10@users.noreply.github.com>
2020-11-10 22:23:31 +0200

    Done HomeWork 10

---
+++ HT_1/HW10.py
@@ -0,0 +1,17 @@
+#10. Write a script to concatenate following dictionaries to create a new one.
+#		Sample Dictionary :
+#		dic1={1:10, 2:20}
+#		dic2={3:30, 4:40}
+#		dic3={5:50,6:60}
+#		Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+
+# My Answer:
+
+dic1 = {1:10, 2:20}
+dic2 = {3:30, 4:40}
+dic3 = {5:50,6:60}
+
+new_dic = dict()
+for i in [dic1,dic2,dic3]:
+	new_dic.update(i)
+print(new_dic)
