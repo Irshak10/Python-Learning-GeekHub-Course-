7b713b33a223ff63c648ffac1fae30c0c9e035a5
Irshak10 <74018714+Irshak10@users.noreply.github.com>
2020-11-10 22:23:50 +0200

    Done HomeWork 13

---
+++ HT_1/HW13.py
@@ -0,0 +1,11 @@
+#13. Write a script to get the maximum and minimum value in a dictionary.
+
+# My Answer:
+
+a = {1: 500, 2: 20, 3: 355, 4: 299, 5: 500, 6: 6000}
+value_min = min(a.values())
+value_max = max(a.values())
+
+print('Мінімальні значення', value_min)
+print('Максимальні значення', value_max)
+
